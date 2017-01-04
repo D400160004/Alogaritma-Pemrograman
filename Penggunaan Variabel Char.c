8679cc62595fe0d21ba8f317bf23290084caec37
D400160004 <adnanmuh0@gmail.com>
2017-01-04 10:19:59 +0700

    Create Penggunaan Variabel Char.c
    sebuah program yg menggunakan variabel char

---
+++ Penggunaan Variabel Char.c
@@ -0,0 +1,33 @@
+\\ Nama: Muhammad Adnan
+\\ NIM : D400160004
+
+#include <stdio.h>
+int main ()
+
+{
+    char nama [200];
+    char warna [200];
+    char hobi [200];
+    char target [200];
+    char cintai [200];
+
+printf ("=================");
+    printf ("\n Nama anda              :");
+    scanf ("%s", nama );
+printf ("=================");
+    printf ( "\nWarna kesuakaan anda    :");
+    scanf (" %s", warna);
+printf ("=================");
+    printf ( "\nHobi anda               :");
+    scanf (" %s", hobi);
+printf ("=================");
+    printf ( "\n Target  anda           :");
+  scanf (" %s", target );
+printf ("=================");
+    printf ("\n orang yang anda  cintai :");
+    scanf (" %s", cintai);
+printf ("==============================================");
+
+    printf ( "\n\n %s warna kesukaan saya %s\n\n dan hobi saya adalah %s .\n\n Target saya adalah masuk %s dan dicintai  %s  \n\n\n\n ",  nama, warna ,  hobi ,target ,cintai  );
+
+return (0);
