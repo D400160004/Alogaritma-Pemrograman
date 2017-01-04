e55d4b3a1a4edc790157adaf4a7a5263ae0fd957
D400160004 <adnanmuh0@gmail.com>
2017-01-04 10:04:17 +0700

    Create penggunaan int & void.c

---
+++ penggunaan int & void.c
@@ -0,0 +1,37 @@
+   #include <stdio.h>
+      #include <conio.h>
+
+
+int main ()
+{
+int a, b ,c ;
+   printf ("\n         MASUKKAN NILAI DENGAN PERMISALAN (a , b , dan c) \n ");
+       printf ( "==============================================================\n");
+            printf ("\n Masukkan Nilai untuk Penjumlahan_Perkalian_Pembagian_Pengurangan : ");
+                scanf ( " %d", a);
+              printf ( "\n=======================================\n");
+        printf ("\n Hasil penjumlahan  dari   :") ;
+    scanf ( " %d",b);
+c= a+b ;
+  printf (" %d + %d = %d\n",a,b,c );
+     scanf ( " %d", a);
+         printf ( "=======================================\n\n");
+            printf (" Hasil  perkalian dari   :") ;
+                scanf ( " %d",b);
+             c=a*b;
+          printf (" %d * %d = %d\n",a,b,c );
+      scanf ( " %d", a);
+   printf ( "======================================\n\n");
+printf (" Hasil pembagian dari  :") ;
+  scanf ( " %d",b);
+     c=a/b;
+        printf (" %d / %d = %d\n",a,b,c );
+           scanf ( " %d", a);
+              printf ( "======================================\n\n");
+            printf (" Hasil  pengurangan dari  :") ;
+         scanf ( " %d",b);
+     c=a-b;
+  printf (" %d - %d = %d\n\n\n",a,b,c );
+return( 0);
+
+}
